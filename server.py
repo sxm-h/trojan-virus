b508e29c17eb8d8e87f25cd4fb8357cb4f13e2fd
Sam Harrison <53513013+sxm-h@users.noreply.github.com>
2021-02-01 10:39:17 +0000

    Add files via upload

---
+++ server.py
@@ -0,0 +1,40 @@
+import socket
+import os
+import threading
+
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+FORMAT = "UTF-8"
+
+HOST = "192.168.0.12"
+PORT = 5050
+
+ADDR = (HOST, PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind(ADDR)
+
+
+print("[LISTENING] Waiting for connections...")
+
+server.listen()
+
+def send(msg):
+	message = msg.encode(FORMAT)
+	conn.send(message)
+
+while True:
+	conn, addr = server.accept()
+	print(f"[NEW CONNECTION] {addr}")
+
+	run = True
+
+	while run:
+		command = input(">>")
+		send(command)
+		print(conn.recv(2000).decode(FORMAT))
+	break
+			
+
+
